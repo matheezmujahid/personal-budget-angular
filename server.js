54e81ed6aee38cf8d6d5b7a8ae378418cca94dc6
matheezmujahid <matheez29@gmail.com>
2023-09-17 16:27:01 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -1,42 +1,22 @@
 const express = require('express');
+const fileSystem = require('fs');
 const app = express();
 const port = 3000;
-
-app.use('/', express.static('public'));
-
-const budget = {
-    myBudget: [
-    {
-        title: 'Eat Out',
-        budget: 30
-    },
-    {
-        title: 'Room Rent',
-        budget: 450
-    },
-    {
-        title: 'Groceries',
-        budget: 90
-    },
-    {
-        title: 'Entertainment',
-        budget: 60
-    },
-    {
-        title: 'Other Expenses',
-        budget: 80
-    }
-    ]
-};
+const importJSON = fileSystem.readFileSync('budgetdata.json', 'utf8');
+const budgetData = JSON.parse(importJSON);
 
 app.get('/hello', (req, res) => {
     res.send('Hello World!');
 });
 
-app.get('/budget', (req, res) => {
-    res.json(budget);
-});
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 });
+
+app.get('/budget', (req, res) => {
+    res.json(budgetData);
+});
+
+
+app.use('/', express.static('public'));
