8f50d3eb1a930aacc297612287304d5ab42e1432
matheezmujahid <matheez29@gmail.com>
2023-09-17 21:28:23 -0400

    Added D3JS chart

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = express();
 const port = 3000;
 const importJSON = fileSystem.readFileSync('budgetdata.json', 'utf8');
 const budgetData = JSON.parse(importJSON);
+const importJSOND3Js = fileSystem.readFileSync('budgetDataD3js.json', 'utf8');
+const budgetDataD3Js = JSON.parse(importJSOND3Js);
 
 app.get('/hello', (req, res) => {
     res.send('Hello World!');
@@ -16,6 +18,9 @@ app.listen(port, () => {
 
 app.get('/budget', (req, res) => {
     res.json(budgetData);
+});
+app.get('/budgetD3js', (req, res) => {
+    res.json(budgetDataD3Js);
 });
 
 
